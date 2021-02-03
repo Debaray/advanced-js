6f532268dd6dbcefd1115f32462265b0110f221a
Debashis Ray <debaray14@gmail.com>
2021-02-03 23:27:39 +0600

    advancedJs

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first+second+bonus;
+    //console.log(bonus);
+    console.log(result);
+    if(result > 9){
+        var  mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky";
+       // console.log(mood);
+    }
+    //console.log(mood);
+    let day = "friday";
+    return result;
+}
+
+const output = sum(3,7);
+console.log(bonus);
+// console.log(day);
+//console.log(output);
