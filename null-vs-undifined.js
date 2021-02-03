fdf2079307dea2dd5c57ad6a80ba36b65b702a32
Debashis Ray <debaray14@gmail.com>
2021-02-03 22:05:38 +0600

    map,filter and find porjonto shes

---
+++ null-vs-undifined.js
@@ -0,0 +1,31 @@
+let pakhi;
+function add(num1,num2)
+{
+    console.log( num1+num2);
+}
+function add1(num1,num2)
+{
+    return;
+}
+function add(num1,num2)
+{
+    console.log(num2);
+}
+console.log(pakhi);
+const result = add(13,82);
+console.log(result);
+const result1 = add1(13,82);
+console.log(result1);
+const result2 = add1(82);
+console.log(result2);
+
+
+const premik= {name:"sddfdf", phone:55613112};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [1,5,2,4];
+console.log(ages[55]);
---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy
+//0
+//""
+//undifined
+//null
+//NaN
+//truthy
+//'0',' ',[]
+const age = 0;
+const name = "0";
+let name1 = {};
+console.log(name1);
+if(name1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
